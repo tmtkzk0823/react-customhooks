e529ddfef85f547be01072cadfcd6ef35d955dac
tomita_kazuki <96096735+tmtkzk0823@users.noreply.github.com>
2022-09-04 00:10:29 +0900

    Add: カスタムフックの呼び出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import "./styles.css";
 
 import { UserCard } from "./components/UserCard";
-import { User } from "./types/api/user";
-import axios from "axios";
-import { UserProfile } from "./types/userProfile";
-import { useState } from "react";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 // const user = {
 //   id: 1,
@@ -14,32 +11,8 @@ import { useState } from "react";
 // };
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const onClickFechUser = () => {
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.street}${user.address.suite}${user.address.city}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFechUser = () => getUsers();
   return (
     <div className="App">
       <button onClick={onClickFechUser}>データ取得</button>
